6c125b4ef36c3605784e411d2e27a344710d0c2a
CNaiko <460078027@qq.com>
2018-10-26 17:22:05 +0800

    [clear] delete comment for poc

--- jsdk/src/index.ts
+++ jsdk/src/index.ts
@@ -2,18 +2,4 @@ import { Prajna } from "./prajna";
 
 module.exports = Prajna;
 
-const p = new Prajna("http://localhost:8545")
-
-// p.contracts.loadDebtKernelAsync().then(x => {
-//     console.log(x)
-//     console.log('-------------------------------11')
-// });
-//
-// p.contracts.loadDharmaContractsAsync().then(x => {
-//     console.log(x)
-//     console.log('-----------------------')
-// });
-
-// p.contracts.loadCreditorProxyAsync().then(x => {
-//     console.log(x)
-// });
+const p = new Prajna("http://localhost:8545");
